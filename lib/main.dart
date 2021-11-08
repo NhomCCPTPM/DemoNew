51a8662ed9221ebbaa7426a439a9e4cfe7c22bfb
LCTien <tienIT21201@gmail.com>
2021-11-07 22:48:47 -0800

    Add Nhan & Update main

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,8 @@ class _MyHomePageState extends State<MyHomePage> {
   PhepTinh _phepTinh = PhepTinh.cong;
   bool isChecked = true;
   double ketQua = 0;
+
+  get txtsothunhat => null;
   @override
   void dispose() {
     txtSoThuNhat.dispose();
@@ -57,120 +59,120 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: SingleChildScrollView(
-          keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-          child: Column(
-            children: [
-              Padding (
-                padding: EdgeInsets.all(8),
-                child: TextField(
-                  decoration: const InputDecoration (
-                    labelText: 'Số thứ nhất',
-                    border: OutlineInputBorder(),
-                    icon: Icon(Icons.star_rate),
-                  ),
-                  keyboardType: TextInputType.number,
-                  textInputAction: TextInputAction.next,
-                  controller: txtsothunhat,
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Số thứ nhất',
+                  border: OutlineInputBorder(),
+                  icon: Icon(Icons.star_rate),
                 ),
+                keyboardType: TextInputType.number,
+                textInputAction: TextInputAction.next,
+                controller: txtsothunhat,
               ),
-              Padding(
-                padding: EdgeInsets.all(8),
-                child: TextField(
-                  decoration: const InputDecoration (
-                    labelText: 'Số thứ hai',
-                    border: OutlineInputBorder(),
-                    icon: Icon(Icons.nightlight_round),
-                  ),
-                  keyboardType: TextInputType.number,
-                  controller: txtsothuhai,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Số thứ hai',
+                  border: OutlineInputBorder(),
+                  icon: Icon(Icons.nightlight_round),
                 ),
+                keyboardType: TextInputType.number,
+                controller: txtSoThuHai,
               ),
-              ListTile(
-                title: const Text('Cộng'),
-                leading: Radio<PhepTinh>(
-                  value: PhepTinh.cong,
-                  groupValue: _pheptinh,
-                  onChanged: (PhepTinh? value) {
-                    setState(() {
-                      _pheptinh = value!;
-                    });
-                  },
+            ),
+            ListTile(
+              title: const Text('Cộng'),
+              leading: Radio<PhepTinh>(
+                value: PhepTinh.cong,
+                groupValue: _phepTinh,
+                onChanged: (PhepTinh? value) {
+                  setState(() {
+                    _phepTinh = value!;
+                  });
+                },
+              ),
+            ),
+            ListTile(
+              title: const Text('Trừ'),
+              leading: Radio<PhepTinh>(
+                value: PhepTinh.tru,
+                groupValue: _phepTinh,
+                onChanged: (PhepTinh? value) {
+                  setState(() {
+                    _phepTinh = value!;
+                  });
+                },
+              ),
+            ),
+            ListTile(
+              title: const Text('Nhân'),
+              leading: Radio<PhepTinh>(
+                value: PhepTinh.nhan,
+                groupValue: _phepTinh,
+                onChanged: (PhepTinh? value) {
+                  setState(() {
+                    _phepTinh = value!;
+                  });
+                },
+              ),
+            ),
+            ListTile(
+              title: const Text('Chia'),
+              leading: Radio<PhepTinh>(
+                value: PhepTinh.chia,
+                groupValue: _phepTinh,
+                onChanged: (PhepTinh? value) {
+                  setState(() {
+                    _phepTinh = value!;
+                  });
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: ElevatedButton(
+                onPressed: () {
+                  var a = double.parse(txtSoThuNhat.text);
+                  var b = double.parse(txtSoThuHai.text);
+                  ketQua = tinhToan(a, b, _phepTinh);
+                  txtKetQua.text = lamTron(ketQua, isChecked);
+                },
+                child: const Text('Kết quả'),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8),
+              child: TextField(
+                readOnly: true,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  icon: Icon(Icons.settings),
                 ),
+                controller: txtKetQua,
               ),
-              ListTile(
-                title: const Text('Trừ'),
-                leading: Radio<PhepTinh>(
-                  value: PhepTinh.tru,
-                  groupValue: _pheptinh,
-                  onChanged: (PhepTinh? value) {
-                    setState(() {
-                      _pheptinh = value!;
-                    });
-                  },
-                ),
+            ),
+            ListTile(
+              leading: Checkbox(
+                value: isChecked,
+                onChanged: (bool? value) {
+                  setState(() {
+                    isChecked = value!;
+                  });
+                  txtKetQua.text = lamTron(ketQua, isChecked);
+                },
               ),
-              ListTile(
-                title: const Text('Nhân'),
-                leading: Radio<PhepTinh>(
-                  value: PhepTinh.nhan,
-                  groupValue: _pheptinh,
-                  onChanged: (PhepTinh? value) {
-                    setState(() {
-                      _pheptinh = value!;
-                    });
-                  },
-                ),
-              ),
-              ListTile(
-                title: const Text('Chia'),
-                leading: Radio<PhepTinh>(
-                  value: PhepTinh.chia,
-                  groupValue: _pheptinh,
-                  onChanged: (PhepTinh? value) {
-                    setState(() {
-                      _pheptinh = value!;
-                    });
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(15),
-                child: ElevatedButton(
-                  onPressed: () {
-                    var a = double.parse(txtsothunhat.text);
-                    var b = double.parse(txtsothuhai.text);
-                    ketqua = tinhToan(a, b, _pheptinh);
-                    txtketqua.text = lamTron(ketqua, isChecked);
-                  },
-                  child: const Text('Kết quả'),
-                ),
-              ),
-              Padding (
-                padding: EdgeInsets.all(8),
-                child: TextField(
-                  readOnly: true,
-                  decoration: const InputDecoration (
-                    border: OutlineInputBorder(),
-                    icon: Icon(Icons.settings),
-                  ),
-                  controller: txtketqua,
-                ),
-              ),
-              ListTile(
-                leading: Checkbox (
-                  value: isChecked,
-                  onChanged: (bool? value){
-                    setState(() {
-                      isChecked = value;
-                    });
-                    txtketqua.text = lamTron(ketqua, isChecked);
-                  },
-                ),
-                title: const Text("Làm tròn số"),
-              ),
-            ],
-          ),
-      ), 
+              title: const Text("Làm tròn số"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
